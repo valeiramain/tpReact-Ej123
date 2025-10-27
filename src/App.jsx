3ad752e4bcea9965e8b71b07afcae65f40c7a92f
Valentina Iramain <valentinairamain2020@gmail.com>
2025-10-27 09:04:03 -0300

    realizamos ejercicios usando props y useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import HolaMundo from "./components/HolaMundo"
+import HolaMundo2 from "./components/holaMundo2"
+import HolaMundo3 from "./components/HolaMundo3"
+
 function App() {
 
-
+  const saludo = 'My Friend'
   return (
     <>
       <HolaMundo></HolaMundo>
+      <HolaMundo2 saludo={saludo}></HolaMundo2>
+      <HolaMundo3 saludo={saludo}></HolaMundo3>
     </>
   )
 }
--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -1,8 +1,7 @@
 
-
 const HolaMundo = () => {
     return (
-        <section className='container'>
+        <section className='container my-5'>
             <h1 className='display-4'>Hello World 🌎!</h1>
         </section>
     );
